531a9bf3e342cbcac5afcc86c5f5dac35c37b344
Sid Natarajan <sid.nat@gmail.com>
2023-01-01 04:20:13 +0000

    exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,13 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+
+puts "Store names before update: #{@store1.name}, #{@store2.name}"
+
+@store1.update(name: "Toronto")
+
+puts "Updated name of store 1: #{@store1.name}"
+
+puts "Updated store names: #{@store1.name}, #{@store2.name}"
